80cb813b1818e425f78782cb14bb23e77b25da31
Mupenzi Herve <mupenziherve04@gmail.com>
2025-08-31 15:16:35 +0200

    saturday

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -6,25 +6,36 @@ import logo from "./logo.svg";
 
 const NavBar = () => {
   const [showLinks, setShowLinks] = useState(false);
-  return <nav>
-     <div className="nav-center">
-    <div className="nav-header">
-      <img src={logo} className="logo" alt="logo"/>
-      <button className="nav-toggle">
-        <FaBars />
-      </button>
-    </div>
-    <div className="links-container">
-      <ul className="links">
-        {links.map((links)=>{
-        const {id, url, text} = link
-        return <li key={id}></li>
 
-      })}
-      </ul>
-    </div>
-    </div>
-  </nav>;
+  const toggleLinks = () => {
+    setShowLinks(!showLinks)
+  };
+  return (
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+             <img src={logo} className="logo" alt="logo" />
+          <button className="nav-toggle" onClick={toggleLinks}>
+            <FaBars />
+          </button>
+        </div>
+        {showLinks && (
+          <div className="links-container">
+            <ul className="links">
+              {links.map((link) => {
+                const { id, url, text } = link
+                return (
+                  <li key={id}>
+                    <a href={url}>{text}</a>
+                  </li>
+                )
+              })}
+            </ul>
+          </div>
+        )};
+      </div>
+    </nav>
+  )
     
 };
 export default NavBar;
